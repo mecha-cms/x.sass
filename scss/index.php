194474e225abe15400ef6458f8fc25fcd6b5a848
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-02-06 09:14:38 +0700

    Update

--- scss/index.php
+++ scss/index.php
@@ -3,8 +3,8 @@
 require __DIR__ . DS . 'engine' . DS . 'i' . DS . '@scssphp' . DS . 'scssphp' . DS . 'scss.inc.php';
 
 require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'asset.php';
-require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'from.php';
-require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'to.php';
+require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'from.php';//
+// require __DIR__ . DS . 'engine' . DS . 'plug' . DS . 'to.php';
 
 require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'file.php';
 require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'hook.php';
