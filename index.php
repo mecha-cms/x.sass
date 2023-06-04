a8613c6692bdb80b66dcc28e7e5a10e3e20cd11d
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-04 20:45:03 +0700

    Naming Convention

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
-<?php
+<?php namespace x\sass;
 
-namespace x\sass {
-    function asset() {
-        // Output is not required in this case, just trigger the asset(s)
-        if (\class_exists("\\Asset")) {
-            \Asset::join('*.sass');
-            \Asset::join('*.scss');
-        }
+require __DIR__ . \D . 'engine' . \D . 'vendor' . \D . 'autoload.php';
+
+function content() {
+    // Output is not required in this case, just trigger the asset(s)
+    if (\class_exists("\\Asset")) {
+        \Asset::join('*.sass');
+        \Asset::join('*.scss');
     }
-    // Make sure to run this hook before `head`
-    \Hook::set('content', __NAMESPACE__ . "\\asset", -1);
 }
 
-namespace {
-    require __DIR__ . \D . 'engine' . \D . 'vendor' . \D . 'autoload.php';
-    if (\defined("\\TEST") && 'x.sass' === \TEST) {
-        \Asset::set(__DIR__ . \D . 'test.scss', 20);
-    }
+// Make sure to run this hook before `head`
+\Hook::set('content', __NAMESPACE__ . "\\content", -1);
+
+if (\defined("\\TEST") && 'x.sass' === \TEST) {
+    \Asset::set(__DIR__ . \D . 'test.scss', 20);
 }
